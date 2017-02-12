dee4dacf80f2b62700101ecd0d524be8ff1ad4fa
mehul4804 <mehul.ghosh4804@gmail.com>
2017-02-12 20:17:50 +0530

    Add files via upload

---
+++ SourceFile.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class SourceFile 
+{
+   public static void main(String[] args) throws Exception 
+{
+      BufferedWriter out1 = new BufferedWriter(new FileWriter("srcfile"));
+      out1.write("File Copied\n");
+      out1.close();
+      InputStream in = new FileInputStream(new File("srcfile"));
+      OutputStream out = new FileOutputStream(new File("destnfile"));
+      byte[] buf = new byte[1024];
+      int len;
+      
+      while ((len = in.read(buf)) > 0) 
+{
+         out.write(buf, 0, len);
+      }
+      in.close();
+      out.close();
+      BufferedReader in1 = new BufferedReader(new FileReader("destnfile"));
+      String str;
+      
+      while ((str = in1.readLine()) != null) 
+{
+         System.out.println(str);
+      }
+      in.close();
+   }
+}
